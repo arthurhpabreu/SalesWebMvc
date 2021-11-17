28195176ecf94e25a69849b213678a28211dcae2
Arthur Henrique <arthurhenriquedepaula@gmail.com>
2021-11-17 10:38:59 -0300

    App locale, number, date formatting and validation

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMvc.Models
@@ -7,11 +8,24 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="{0} required")]
         public string Name { get; set; }
-        public string Email { get; set; }
-        public DateTime BirthDate { get; set; }
-        public double BaseSalary { get; set; }
 
+        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
+        public string Email { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} required")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [Required(ErrorMessage = "{0} required")]
+        [DisplayFormat(DataFormatString ="{0:f2}")]
+        public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
